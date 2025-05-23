b8ae504b7cf4f2815d9df3a47479f08087195a8c
liuzhibiao <1282807444@qq.com>
2025-05-23 17:43:08 +0800

    111

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.anhe.khmall',
   appName: 'ionic-app-base',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+    iosScheme: 'https',
+  },
 };
 
 export default config;
